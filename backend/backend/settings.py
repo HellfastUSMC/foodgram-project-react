e34faa94ac6fc15f81019d9c2c924fbb4cbf83ec
Alexander Nabiev <4epenki@gmail.com>
2022-06-13 22:47:15 +0300

    another try

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,12 +98,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '5432')
+        'ENGINE': os.getenv('DB_ENGINE'),# 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME'),# 'postgres'),
+        'USER': os.getenv('POSTGRES_USER'),#, 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),#, 'postgres'),
+        'HOST': os.getenv('DB_HOST'),#, '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT'),#, '5432')
     }
 }
 
