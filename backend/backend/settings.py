14e3b3b34adae1349c86365a08a44b1c2c07170a
HellfastUSMC <4epenki@gmail.com>
2022-06-14 13:42:58 +0300

    sdfhgfdsdg

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', default='12345')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
