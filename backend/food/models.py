b18de289723c8c1612bf7297f568bbc2f42f93c3
HellfastUSMC <4epenki@gmail.com>
2022-06-16 15:28:11 +0300

    trying to fix images

--- backend/food/models.py
+++ backend/food/models.py
@@ -47,7 +47,6 @@ class Recipe(models.Model):
     name = models.CharField('Название', max_length=200)
     image = models.ImageField(
         'Обложка',
-        upload_to='recipes/images',
         max_length=5500000
     )
     text = models.TextField('Описание', max_length=1000)
