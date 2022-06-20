1a9ce16b84ee8630b658e8e857b197588f000c80
HellfastUSMC <4epenki@gmail.com>
2022-06-20 11:11:36 +0300

    mReview2_another_try_images

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -168,9 +168,9 @@ class RecipeSerializer(serializers.ModelSerializer):
                 instance.tags.all(),
                 many=True
             ).data
-        if 'image' in self.fields:
-            print(instance.image.url)
-            representation['image'] = str(instance.image.url)
+        # if 'image' in self.fields:
+        #     print(instance.image.url)
+        #     representation['image'] = str(instance.image.url)
         return representation
 
     def validate_image(self, data):
