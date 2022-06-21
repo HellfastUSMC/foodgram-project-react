bdb2c831858ecf524f4479c584c2c35ed6c4ffb0
HellfastUSMC <4epenki@gmail.com>
2022-06-21 15:04:28 +0300

    Final1

--- backend/food/management/commands/load_data.py
+++ backend/food/management/commands/load_data.py
@@ -1,16 +1,13 @@
 import csv
 
 from django.core.management import BaseCommand
-
 from food.models import Product
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-
         print('Начинаем загрузку данных')
-
         with open('./data/ingredients.csv') as file:
             reader = csv.reader(file)
             for row in reader:
